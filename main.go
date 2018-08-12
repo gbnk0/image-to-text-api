37ee524c6fd648f8ebbb0c28be7661a9513f58fc
gbnk0 <michael.amara2@gmail.com>
2018-08-12 23:55:54 +0200

    Add /version and removed file save on disk

--- main.go
+++ main.go
@@ -1,44 +1,73 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"net/http"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/otiai10/gosseract"
 )
 
+func getText(image []byte) string {
+	client := gosseract.NewClient()
+	defer client.Close()
+
+	client.SetImageFromBytes(image)
+	text, _ := client.Text()
+	return text
+}
+
+func getVersion() string {
+	versionFile := "version.txt"
+	version := "dev"
+	if _, err := os.Stat(versionFile); err == nil {
+
+		b, err := ioutil.ReadFile(versionFile)
+		if err != nil {
+			fmt.Print(err)
+		}
+		version = string(b)
+	}
+	return version
+}
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
 func main() {
 	r := gin.Default()
 
 	r.POST("/text", func(c *gin.Context) {
-		file, err := c.FormFile("file")
+		file, _, err := c.Request.FormFile("file")
+		check(err)
+		defer file.Close()
 
-		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, file); err != nil {
+			check(err)
 			return
 		}
 
-		if err := c.SaveUploadedFile(file, file.Filename); err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
-			return
-		}
-		text := getText(file.Filename)
+		text := getText(buf.Bytes())
 		c.JSON(200, gin.H{
 			"status": "success",
 			"text":   text,
 		})
 
-		os.Remove(file.Filename)
+	})
+
+	r.GET("/version", func(c *gin.Context) {
+		version := getVersion()
+		c.JSON(200, gin.H{
+			"status":  "success",
+			"version": version,
+		})
+
 	})
 	r.Run(":8000")
-}
-
-func getText(imagePath string) string {
-	client := gosseract.NewClient()
-	defer client.Close()
-	client.SetImage(imagePath)
-	text, _ := client.Text()
-	return text
 }
